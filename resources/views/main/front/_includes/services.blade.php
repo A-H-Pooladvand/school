b3b9cbafe64b9eafa5890f12d1e0433f7ff47fca
Amirhossein Pooladvand <a.h.pooladvand@gmail.com>
2019-04-08 11:28:09 +0430

    Fixes

--- resources/views/main/front/_includes/services.blade.php
+++ resources/views/main/front/_includes/services.blade.php
@@ -1,4 +1,4 @@
-<section id="about" >
+<section id="about">
     <div class="container">
 
         <h2 class="heading heading_space wow fadeInDown">
@@ -6,18 +6,22 @@
             <span class="divider-left"></span>
         </h2>
 
-            <div class="container">
-                <div class="row">
-                    <div class="clearfix">
-                        @foreach($services as $service)
-                            <div class="col-sm-4 icon_box text-center wow fadeInUp" data-wow-delay="300ms">
+        <div class="container">
+            <div class="row">
+                <div class="clearfix">
+                    @foreach($services as $service)
+                        <div class="col-sm-4 icon_box text-center wow fadeInUp" data-wow-delay="300ms">
+                            <a href="{{ route('service.show', $service->id) }}">
                                 <img src="{{ image_url($service->image, 6,6, true) }}" alt="{{ $service->title }}">
+                            </a>
+                            <a href="{{ route('service.show', $service->id) }}">
                                 <h4 class="text-capitalize bottom20 margin10">{{ $service->title }}</h4>
-                                <p class="no_bottom">{{ str_limit($service->summary, 90) }}</p>
-                            </div>
-                        @endforeach
-                    </div>
+                            </a>
+                            <p class="no_bottom">{{ str_limit($service->summary, 90) }}</p>
+                        </div>
+                    @endforeach
                 </div>
             </div>
+        </div>
     </div>
 </section>
